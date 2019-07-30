12d3b8486a9798bd185425bdf818ef2048867bb1
skywalker512 <houzhenhong@hotmail.com>
2019-07-30 10:20:39 +0800

    refactor: 热门标签抽离出来

--- src/components/SearchHot.vue
+++ src/components/SearchHot.vue
@@ -1,17 +1,16 @@
 <template>
-  <div class="hot">
-    <div class="title">热门搜索</div>
-    <div class="content">
-      <div class="item" v-for="item of searchHot" :key="item.id">{{item.name}}</div>
-    </div>
-  </div>
+  <VHotTag name="热门搜索" :hotTagData="searchHot"/>
 </template>
 
 <script>
 import { mapGetters } from "vuex"
 import { FETCH_SEARCH_HOT } from "@/store/type/actions"
+import VHotTag from '@/components/VHotTag'
 
 export default {
+  components: {
+    VHotTag
+  },
   mounted() {
     this.$store.dispatch(FETCH_SEARCH_HOT)
   },
@@ -24,29 +23,4 @@ export default {
 
 
 <style lang="less" scoped>
-.hot {
-  .title {
-    margin: 30px 0 0 28px;
-    font-size: 28px;
-    color: #9e9e9f;
-  }
-
-  .content {
-    margin: 0 16px 0 16px;
-    display: flex;
-    flex-wrap: wrap;
-    .item {
-      height: 44px;
-      line-height: 44px;
-      border-radius: 44px;
-      margin: 28px 12px 0 12px;
-      padding: 0 20px 0 20px;
-      background-color: #ffffff;
-      border: 2px solid @mainColor;
-      box-sizing: border-box;
-      font-size: 28px;
-      color: @mainColor;
-    }
-  }
-}
 </style>
