931b092984755692fcfaf23074c7814d42e1290d
skywalker512 <houzhenhong@hotmail.com>
2019-08-05 19:43:35 +0800

    fix: vue-router bug

--- src/views/HomeTag.vue
+++ src/views/HomeTag.vue
@@ -23,24 +23,19 @@ export default {
   components: {
     HomeEditButton,
   },
-  watch: {
-    $route (to) {
-      this.$store.dispatch(FETCH_QUESTION_BY_TAG, to.query.id)
-    }
-  },
   computed: {
     ...mapGetters(['tagHot', 'isLoading', 'questionList']),
     tagName() {
       let tagName = ''
       this.tagHot.forEach(element => {
-        if(element.id === this.$route.query.id) {
+        if(element.id === Number(this.$route.query.id)) {
           tagName = element.name
         }
       });
       return `${tagName} 分类下的问题`
     }
   },
-  mounted () {
+  created () {
     this.$store.dispatch(FETCH_QUESTION_BY_TAG, this.$route.query.id)
   }
 }
