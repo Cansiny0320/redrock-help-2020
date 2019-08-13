b1b934c95b005ec4f823fb3f2deab18cdaa13d6a
skywalker512 <houzhenhong@hotmail.com>
2019-08-13 19:10:31 +0800

    fix: 解决时区问题

--- src/common/filter/date.js
+++ src/common/filter/date.js
@@ -1,6 +1,7 @@
 import { format } from 'timeago.js'
 
 export default date => {
-  let time = new Date(date)
+  // 解决时区问题
+  const time = new Date(date).getTime() + 8 * 1000 * 60 * 60
   return format(time, 'zh_CN')
 }
