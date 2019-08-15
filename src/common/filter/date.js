e3fadb4b331446cd1f622fb2c02573f07db09473
skywalker512 <houzhenhong@hotmail.com>
2019-08-15 16:27:02 +0800

    fix: ios 上面 getTime 问题

--- src/common/filter/date.js
+++ src/common/filter/date.js
@@ -2,6 +2,14 @@ import { format } from 'timeago.js'
 
 export default date => {
   // 解决时区问题
-  const time = new Date(date).getTime() + 8 * 1000 * 60 * 60
+  let u = navigator.userAgent;   //判断浏览器型号
+  let isiOS = !!u.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/); //ios终端
+  let time
+  if (isiOS) {
+    time = new Date((date).replace(/-/g, '/')).getTime();
+  } else {
+    time = new Date(date).getTime();
+  }
+  const time = time + 8 * 1000 * 60 * 60
   return format(time, 'zh_CN')
 }
