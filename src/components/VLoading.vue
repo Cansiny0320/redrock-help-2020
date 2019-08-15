b9ff100f0669342f8858089450a773a81d1128ab
skywalker512 <houzhenhong@hotmail.com>
2019-08-15 17:29:57 +0800

    fix: 重新加入 loading

--- src/components/VLoading.vue
+++ src/components/VLoading.vue
@@ -1,26 +1,25 @@
 <template>
-
-  <div></div>
-
+  <div class="v-loading"> 
+    <ImageLoadingSvg />
+  </div>
 </template>
 
 <script>
+import ImageLoadingSvg from '@/assets/svg/ImageLoading.svg'
 export default {
   name: 'vLoading',
-  props: {
-    number: {
-      type: Number,
-      required: false,
-      default: 4,
-    }
-  },
-  data () {
-    return {
-      array: Array(this.number)
-    }
+  components: {
+    ImageLoadingSvg,
   }
 }
 </script>
 
 <style lang="less" scoped>
+.v-loading {
+  width: 100%;
+  height: 200px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
 </style>
