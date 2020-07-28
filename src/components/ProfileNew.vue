2310d15b9f1a60379c6faf8a847f78bdfe16a5c0
Cansiny0320 <1136843910@qq.com>
2020-07-28 15:48:54 +0800

    feat: 我的页面修改

--- src/components/ProfileNew.vue
+++ src/components/ProfileNew.vue
@@ -1,60 +1,56 @@
 <template>
-  <div class="profile-new">
-    <div class="user">
-      <img
-        class="img"
-        :src="profileData.author.avatar | https"
-      />
-      <span class="name">{{profileData.author.name}}</span>
+    <div class="profile-new profile-item">
+        <div class="user">
+            <img class="img" :src="profileData.avatar | https" />
+            <div class="info">
+                <div class="name">{{ profileData.name }}</div>
+                <div class="id">统一认证码：{{ profileData.id }}</div>
+            </div>
+        </div>
+
+        <div
+            class="question"
+            @click="handleProfileQuestionClick(profileData.id)"
+        >
+            <span>我提过的问题 {{ profileData.questionsNum }} 个</span>
+            <div class="front-icon">
+                <BaseFront />
+            </div>
+        </div>
+        <div class="comment" @click="handleProfileAnswerClick('comment')">
+            <span>我收到的回答 {{ profileData.answersNum }} 个</span>
+            <span class="new">有{{ profileData.newAnswerNum }}条未读消息</span>
+            <div class="front-icon">
+                <BaseFront />
+            </div>
+        </div>
     </div>
-
-    <div
-      class="question"
-      @click="handleProfileQuestionClick(profileData.id)"
-    >
-      <span>我提过的问题 {{profileData.author.questionsNum}} 个</span>
-      <div class="front-icon">
-        <BaseFront />
-      </div>
-    </div>
-
-    <!-- <div
-      class="comment"
-      @click="handleProfileAnswerClick('comment')"
-    >
-      <span>我收到的评论 {{profileData.answersNum}} 个</span>
-      <div class="front-icon">
-        <BaseFront />
-      </div>
-    </div> -->
-
-  </div>
 </template>
 
 <script>
 import BaseFront from "@/assets/svg/BaseFront.svg";
 export default {
-  name: "profileNew",
-  components: {
-    BaseFront
-  },
-  props: {
-    profileData: {
-      type: Object,
-      required: true
+    name: "profileNew",
+    components: {
+        BaseFront
+    },
+    props: {
+        profileData: {
+            type: Object,
+            required: true
+        }
+    },
+    methods: {
+        handleProfileQuestionClick(id) {
+            this.$router.push({ name: 'profileQuestion', query: { id } })
+        },
+        handleProfileAnswerClick(type) {
+            this.$router.push({ name: 'profileAnswer', query: { type } })
+        },
     }
-  },
-  methods: {
-    handleProfileQuestionClick (id) {
-      this.$router.push({ name: 'profileQuestion', query: { id } })
-    },
-    handleProfileAnswerClick (type) {
-      this.$router.push({ name: 'profileAnswer', query: { type } })
-    },
-  }
 };
 </script>
 
 <style lang="less" scoped>
-@import "../assets/styles/profile.less";
+@import '../assets/styles/profile.less';
 </style>
--- src/components/ProfileOld.vue
+++ src/components/ProfileOld.vue
@@ -1,62 +1,55 @@
 <template>
-  <div class="new">
+    <div class="new profile-item">
+        <div class="user">
+            <img class="img" :src="profileData.author.avatar | https" />
+            <div class="info">
+                <div class="name">{{ profileData.author.name }}</div>
+                <div class="id">统一认证码：{{ profileData.id }}</div>
+            </div>
+        </div>
+        <div class="answer" @click="handleClick('answers')">
+            <span>我回答的问题： {{ profileData.author.answersNum }} 个</span>
+            <div class="front-icon">
+                <BaseFront />
+            </div>
+        </div>
+        <div class="approval" @click="handleClick('approval')">
+            <span>我收到的赞同： {{ profileData.number.approvalNum }} 个</span>
+            <div class="front-icon">
+                <BaseFront />
+            </div>
+        </div>
 
-    <div class="user">
-      <img class="img" :src="profileData.author.avatar | https"/>
-      <span class="name">{{profileData.author.name}}</span>
+        <!-- <div class="oppose" @click="handleClick('oppose')">
+            <span>我收到的反对 {{ profileData.number.opposeNum }} 个</span>
+            <div class="front-icon">
+                <BaseFront />
+            </div>
+        </div> -->
     </div>
-
-    <div 
-    class="approval"
-    @click="handleClick('approval')">
-      <span>我收到的赞同 {{profileData.number.approvalNum}} 个</span>
-      <div class="front-icon">
-        <BaseFront />
-      </div>
-    </div>
-
-    <div 
-    class="oppose"
-    @click="handleClick('oppose')">
-      <span>我收到的反对 {{profileData.number.opposeNum}} 个</span>
-      <div class="front-icon">
-        <BaseFront />
-      </div>
-    </div>
-
-    <div 
-    class="answer"
-    @click="handleClick('answers')">
-      <span>我的回答 {{profileData.author.answersNum}} 个</span>
-      <div class="front-icon">
-        <BaseFront />
-      </div>
-    </div>
-
-  </div>
 </template>
 
 <script>
 import BaseFront from "@/assets/svg/BaseFront.svg";
 export default {
-  name: "ProfileOld",
-  components: {
-    BaseFront,
-  },
-  props: {
-    profileData: {
-      type: Object,
-      required: true
+    name: "ProfileOld",
+    components: {
+        BaseFront,
+    },
+    props: {
+        profileData: {
+            type: Object,
+            required: true
+        }
+    },
+    methods: {
+        handleClick(type) {
+            this.$router.push({ name: 'profileAnswer', query: { type } })
+        }
     }
-  },
-  methods:{
-     handleClick(type) {
-      this.$router.push({ name: 'profileAnswer', query: { type } })
-    }
-  }
 };
 </script>
 
 <style lang="less" scoped>
-@import "../assets/styles/profile.less";
+@import '../assets/styles/profile.less';
 </style>
