3cad35c810dce86f1df200ed0f9c6d8cafbe7f1a
Cansiny0320 <1136843910@qq.com>
2020-07-30 15:19:25 +0800

    fix: 修复最新页面无法获取更多的bug

--- src/mixin/fectchMore.js
+++ src/mixin/fectchMore.js
@@ -1,24 +1,32 @@
-import throttle from '@/common/throttle'
-import { mapGetters } from "vuex"
+import throttle from '@/common/throttle';
+import { mapGetters } from 'vuex';
 
 export default {
-  data () {
+  data() {
     return {
       throttleFunction: null,
-    }
+    };
   },
   mounted() {
-    this.throttleFunction = throttle(() => {
-      if ((window.innerHeight + window.scrollY) >= (document.body.offsetHeight) && !this.isLoadingMore) {
-        this.handelFecthMore();
-      }
-    }, 100, 200)
+    this.throttleFunction = throttle(
+      () => {
+        if (
+          window.innerHeight + window.scrollY + 10 >=
+            document.body.offsetHeight &&
+          !this.isLoadingMore
+        ) {
+          this.handelFecthMore();
+        }
+      },
+      100,
+      200,
+    );
     window.addEventListener('scroll', this.throttleFunction, false);
   },
-  destroyed () {
+  destroyed() {
     window.removeEventListener('scroll', this.throttleFunction, false);
   },
   computed: {
     ...mapGetters(['isLoadingMore']),
   },
-}
+};
