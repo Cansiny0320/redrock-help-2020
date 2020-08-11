406ea102449a3d8d82a04a7bebcad9db7ca814c3
Cansiny0320 <1136843910@qq.com>
2020-08-11 13:22:53 +0800

    fix: 回答数

--- src/components/ProfileQuestion.vue
+++ src/components/ProfileQuestion.vue
@@ -1,52 +1,50 @@
 <template>
-    <div class="profile-question">
-        <div class="box" v-for="item of questionData" :key="item.id">
-            <div class="fix">
-                <div
-                    class="content"
-                    @click="handelQuestionClick(item.id)"
-                    v-text="item.content"
-                ></div>
-                <div class="tags">
-                    <span v-for="item of item.tags.slice(0, 2)" :key="item.id"
-                        >#{{ item.name }}#</span
-                    >
-                </div>
-            </div>
-            <div class="tips">
-                <span class="time">{{ item.createdAt | date }}</span>
-                <span class="comment">
-                    <i class="icon"></i>{{ item.answersCount }}</span
-                >
-                <!-- <span class="delete" @click="handelDeleteClick(item.id)"
+  <div class="profile-question">
+    <div class="box" v-for="item of questionData" :key="item.id">
+      <div class="fix">
+        <div
+          class="content"
+          @click="handelQuestionClick(item.id)"
+          v-text="item.content"
+        ></div>
+        <div class="tags">
+          <span v-for="item of item.tags.slice(0, 2)" :key="item.id"
+            >#{{ item.name }}#</span
+          >
+        </div>
+      </div>
+      <div class="tips">
+        <span class="time">{{ item.createdAt | date }}</span>
+        <span class="comment"> <i class="icon"></i>{{ item.answerCount }}</span>
+        <!-- <span class="delete" @click="handelDeleteClick(item.id)"
                     >删除</span
                 > -->
-                <span
-                    class="solve"
-                    @click="handelSolveClick(item.id)"
-                    v-if="item.status === '未解决' || item.status === '0'"
-                    >确认解决</span
-                >
-                <span
-                    class="solved"
-                    v-if="item.status === '1' || item.status === '已解决'"
-                    >已解决</span
-                >
-            </div>
-        </div>
-        <VToast
-            v-if="isShowDeleteToast"
-            :massage="deleteTipmassage"
-            @confirm="handelDeleteConfirm"
-            @cancel="handelDeleteCancel"
-        />
-        <VToast
-            v-if="isShowSolveToast"
-            :massage="SolveTipmassage"
-            @confirm="handelSolveConfirm"
-            @cancel="handelSolveCancel"
-        />
+        <span
+          class="solve"
+          @click="handelSolveClick(item.id)"
+          v-if="item.status === '未解决' || item.status === '0'"
+          >确认解决</span
+        >
+        <span
+          class="solved"
+          v-if="item.status === '1' || item.status === '已解决'"
+          >已解决</span
+        >
+      </div>
     </div>
+    <VToast
+      v-if="isShowDeleteToast"
+      :massage="deleteTipmassage"
+      @confirm="handelDeleteConfirm"
+      @cancel="handelDeleteCancel"
+    />
+    <VToast
+      v-if="isShowSolveToast"
+      :massage="SolveTipmassage"
+      @confirm="handelSolveConfirm"
+      @cancel="handelSolveCancel"
+    />
+  </div>
 </template>
 
 <script>
@@ -111,61 +109,61 @@ export default {
 
 <style lang="less" scoped>
 .box {
-    background: #ffffff;
-    margin: 20px 24px;
-    padding: 28px 24px;
-    border-radius: 10px;
-    .fix {
-        margin-bottom: 31px;
-        .content {
-            margin-bottom: 15px;
-            font-size: 32px;
-            color: @fontColor;
-            @nowrap();
-        }
-        .tags {
-            font-size: 26px;
-            color: @mainColor;
-            span {
-                margin-right: 10px;
-            }
-        }
+  background: #ffffff;
+  margin: 20px 24px;
+  padding: 28px 24px;
+  border-radius: 10px;
+  .fix {
+    margin-bottom: 31px;
+    .content {
+      margin-bottom: 15px;
+      font-size: 32px;
+      color: @fontColor;
+      @nowrap();
     }
-    .tips {
+    .tags {
+      font-size: 26px;
+      color: @mainColor;
+      span {
+        margin-right: 10px;
+      }
+    }
+  }
+  .tips {
+    display: flex;
+    align-items: center;
+    span {
+      color: #808080;
+      font-size: 22px;
+      &.comment {
         display: flex;
+        margin-left: 48px;
         align-items: center;
-        span {
-            color: #808080;
-            font-size: 22px;
-            &.comment {
-                display: flex;
-                margin-left: 48px;
-                align-items: center;
-                .icon {
-                    display: block;
-                    width: 19px;
-                    height: 19px;
-                    background-image: url('../assets/images/comment.png');
-                    background-repeat: no-repeat;
-                    margin-right: 9px;
-                    background-size: 100% 100%;
-                }
-            }
+        .icon {
+          display: block;
+          width: 19px;
+          height: 19px;
+          background-image: url('../assets/images/comment.png');
+          background-repeat: no-repeat;
+          margin-right: 9px;
+          background-size: 100% 100%;
         }
-        .solve {
-            margin-left: auto;
-            font-size: 24px;
-            color: @mainColor;
-            border: 1px solid @mainColor;
-            border-radius: 300px;
-            padding: 13px 24px;
-        }
-        .solved {
-            font-size: 24px;
-            color: #808080;
-            margin-left: auto;
-            padding: 13px 0px;
-        }
+      }
     }
+    .solve {
+      margin-left: auto;
+      font-size: 24px;
+      color: @mainColor;
+      border: 1px solid @mainColor;
+      border-radius: 300px;
+      padding: 13px 24px;
+    }
+    .solved {
+      font-size: 24px;
+      color: #808080;
+      margin-left: auto;
+      padding: 13px 0px;
+    }
+  }
 }
 </style>
