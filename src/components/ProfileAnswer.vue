d28136049d7bb264129b081c75c3d06bede07c65
Cansiny0320 <1136843910@qq.com>
2020-08-11 11:29:36 +0800

    fix: 发布问题之后的跳转 删除问题

--- src/components/ProfileAnswer.vue
+++ src/components/ProfileAnswer.vue
@@ -4,7 +4,7 @@
       class="box"
       v-for="item in commentData"
       :key="item.ansId"
-      @click="handelQuestionClick(item.questions.queId)"
+      @click="handelQuestionClick(item.questions.id)"
     >
       <div class="question">{{ item.questions.content }}</div>
       <div class="comment">{{ item.content }}</div>
@@ -23,7 +23,7 @@
             <span class="num">{{ item.approvalNum }}</span>
           </div>
         </div>
-        <div class="delete" @click.stop="handelDeleteClick(item.ansId)">
+        <div class="delete" @click.stop="handelDeleteClick(item.id)">
           删除
         </div>
       </div>
