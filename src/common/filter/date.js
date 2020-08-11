6d72f734926fa2ebbe4785e7883c98bfb7c9cc80
Cansiny0320 <1136843910@qq.com>
2020-08-11 19:59:37 +0800

    fix: 避免出现时间在片刻后的情况

--- src/common/filter/date.js
+++ src/common/filter/date.js
@@ -4,7 +4,7 @@ export default date => {
   // 解决时区问题
   // let u = navigator.userAgent; //判断浏览器型号
   // let isiOS = !!u.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/); //ios终端
-  const time = new Date(date.replace(/-/g, '/')).getTime();
+  const time = new Date(date.replace(/-/g, '/')).getTime() - 1000; //避免出现片刻后的情况
   // time = time + 8 * 1000 * 60 * 60;
   return format(time, 'zh_CN');
 };
