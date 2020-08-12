8a8b41d88e3178f56e7bb74aef73571d684d6059
Cansiny0320 <1136843910@qq.com>
2020-08-12 17:47:49 +0800

    fix: footer position

--- src/components/TheFooterNavBar.vue
+++ src/components/TheFooterNavBar.vue
@@ -1,34 +1,34 @@
 <template>
-    <nav class="footer">
-        <router-link
-            :to="{ name: 'homeHot' }"
-            class="item"
-            :class="{ active: isHome }"
-        >
-            <div class="icon" v-if="isHome">
-                <img src="../assets/images/NavBarHomeActive.png" alt="" />
-            </div>
-            <div class="icon" v-else>
-                <img src="../assets/images/NavBarHome.png" />
-            </div>
-            <div class="text">首页</div>
-        </router-link>
-        <router-link
-            :to="{ name: 'profile' }"
-            class="item"
-            :class="{ active: isProfile }"
-        >
-            <div class="icon" v-if="isProfile">
-                <img src="../assets/images/NavBarProfileActive.png" alt="" />
-                <div class="dot" v-if="hasNew"></div>
-            </div>
-            <div class="icon" v-else>
-                <img src="../assets/images/NavBarProfile.png" alt="" />
-                <div class="dot" v-if="hasNew"></div>
-            </div>
-            <div class="text">我的</div>
-        </router-link>
-    </nav>
+  <nav class="footer">
+    <router-link
+      :to="{ name: 'homeHot' }"
+      class="item"
+      :class="{ active: isHome }"
+    >
+      <div class="icon" v-if="isHome">
+        <img src="../assets/images/NavBarHomeActive.png" alt="" />
+      </div>
+      <div class="icon" v-else>
+        <img src="../assets/images/NavBarHome.png" />
+      </div>
+      <div class="text">首页</div>
+    </router-link>
+    <router-link
+      :to="{ name: 'profile' }"
+      class="item"
+      :class="{ active: isProfile }"
+    >
+      <div class="icon" v-if="isProfile">
+        <img src="../assets/images/NavBarProfileActive.png" alt="" />
+        <div class="dot" v-if="hasNew"></div>
+      </div>
+      <div class="icon" v-else>
+        <img src="../assets/images/NavBarProfile.png" alt="" />
+        <div class="dot" v-if="hasNew"></div>
+      </div>
+      <div class="text">我的</div>
+    </router-link>
+  </nav>
 </template>
 
 <script>
@@ -59,52 +59,53 @@ export default {
 
 <style lang="less" scoped>
 .footer {
-    position: fixed;
-    bottom: 0;
-    left: 0;
-    right: 0;
+  position: fixed;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  height: 98px;
+  box-sizing: border-box;
+  z-index: 10;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: #ffffff;
+  box-shadow: 0px 0px 15px 0px rgba(118, 118, 242, 0.1);
+  .item {
     height: 98px;
-    box-sizing: border-box;
-    z-index: 10;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    background-color: #ffffff;
-    box-shadow: 0px 0px 15px 0px rgba(118, 118, 242, 0.1);
-    .item {
-        width: 100%;
-        text-align: center;
-        font-size: 20px;
-        color: @fontColor;
-        padding-top: 12px;
-        .text {
-            font-weight: bold;
-        }
-        &.active {
-            color: @mainColor;
-            path {
-                fill: @mainColor;
-            }
-        }
+    width: 100%;
+    text-align: center;
+    font-size: 20px;
+    color: @fontColor;
+    padding-top: 12px;
+    .text {
+      font-weight: bold;
     }
-    .icon {
-        display: inline-block;
-        position: relative;
-        width: 39px;
-        height: 41px;
-        margin-bottom: 10px;
-        img {
-            width: 100%;
-        }
-        .dot {
-            position: absolute;
-            width: 14px;
-            height: 15px;
-            background: rgba(254, 103, 96, 1);
-            border-radius: 50%;
-            top: -11px;
-            right: -9px;
-        }
+    &.active {
+      color: @mainColor;
+      path {
+        fill: @mainColor;
+      }
     }
+  }
+  .icon {
+    display: inline-block;
+    position: relative;
+    width: 39px;
+    height: 41px;
+    margin-bottom: 10px;
+    img {
+      width: 100%;
+    }
+    .dot {
+      position: absolute;
+      width: 14px;
+      height: 15px;
+      background: rgba(254, 103, 96, 1);
+      border-radius: 50%;
+      top: -11px;
+      right: -9px;
+    }
+  }
 }
 </style>
