f6d8b95ebca5d41ac753c67516a22ee316211354
Cansiny0320 <1136843910@qq.com>
2020-08-17 12:06:01 +0800

    fix: 固定头部高度

--- src/components/HomeHeader.vue
+++ src/components/HomeHeader.vue
@@ -1,18 +1,18 @@
 <template>
-    <header class="home-header">
-        <router-link
-            class="item"
-            :class="{ active: !isNew }"
-            :to="{ name: 'homeHot' }"
-            >最热问题</router-link
-        >
-        <router-link
-            class="item"
-            :class="{ active: isNew }"
-            :to="{ name: 'homeNew' }"
-            >最新问题</router-link
-        >
-    </header>
+  <header class="home-header">
+    <router-link
+      class="item"
+      :class="{ active: !isNew }"
+      :to="{ name: 'homeHot' }"
+      >最热问题</router-link
+    >
+    <router-link
+      class="item"
+      :class="{ active: isNew }"
+      :to="{ name: 'homeNew' }"
+      >最新问题</router-link
+    >
+  </header>
 </template>
 
 <script>
@@ -42,39 +42,40 @@ export default {
 
 <style lang="less" scoped>
 .home-header {
-    position: fixed;
-    z-index: 999;
-    top: 0;
-    right: 0;
-    left: 0;
-    display: flex;
-    justify-content: space-between;
-    background-color: @HeaderBgColor;
-    box-sizing: border-box;
-    border-radius: 2px;
-    padding: 0 131px;
-    .item {
-        font-size: 36px;
-        line-height: 71px;
-        color: #555;
-        letter-spacing: 1px;
-        font-weight: 500;
-        &.active {
-            position: relative;
-            color: @fontColor;
-            font-weight: bold;
-            &::after {
-                content: '';
-                position: absolute;
-                bottom: 0;
-                left: 50%;
-                transform: translateX(-50%);
-                width: 91px;
-                height: 4px;
-                border-radius: 2px;
-                background: rgba(118, 118, 242, 1);
-            }
-        }
+  position: fixed;
+  z-index: 999;
+  top: 0;
+  right: 0;
+  left: 0;
+  display: flex;
+  justify-content: space-between;
+  background-color: @HeaderBgColor;
+  box-sizing: border-box;
+  border-radius: 2px;
+  padding: 0 131px;
+  height: 70px;
+  .item {
+    font-size: 36px;
+    line-height: 71px;
+    color: #555;
+    letter-spacing: 1px;
+    font-weight: 500;
+    &.active {
+      position: relative;
+      color: @fontColor;
+      font-weight: bold;
+      &::after {
+        content: '';
+        position: absolute;
+        bottom: 0;
+        left: 50%;
+        transform: translateX(-50%);
+        width: 91px;
+        height: 4px;
+        border-radius: 2px;
+        background: rgba(118, 118, 242, 1);
+      }
     }
+  }
 }
 </style>
